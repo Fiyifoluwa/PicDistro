08d6fbbb10ab4a4f41c69c2e2efbdf3cad795648
fiyifoluwa <fiyifoluwa@outlook.com>
2021-03-28 08:52:06 +0100

    new firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/storage';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyBRSTFF3rj5o5_lWpGzlmBgkRXAeS0vycc",
-  authDomain: "picdistro-4e724.firebaseapp.com",
-  projectId: "picdistro-4e724",
-  storageBucket: "picdistro-4e724.appspot.com",
-  messagingSenderId: "865854966704",
-  appId: "1:865854966704:web:ee85d6900e598c8b292475"
+  apiKey: "AIzaSyDfcC_Ygaw0WG7OMnKijBCmQauPNa85teI",
+  authDomain: "picdistro-e26a9.firebaseapp.com",
+  projectId: "picdistro-e26a9",
+  storageBucket: "picdistro-e26a9.appspot.com",
+  messagingSenderId: "616633807856",
+  appId: "1:616633807856:web:ef9e632f8369635b396335",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -18,4 +18,4 @@ const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export {projectStorage, projectFirestore, timestamp};
+export { projectStorage, projectFirestore, timestamp };
